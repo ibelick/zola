f9c2f1d17def9712769356b43f39eaa25415177f
ibelick <julien.thibeaut@gmail.com>
2025-04-16 18:17:11 +0200

    feat: update prose MessageContent

--- app/components/chat/message-assistant.tsx
+++ app/components/chat/message-assistant.tsx
@@ -41,7 +41,10 @@ export function MessageAssistant({
     >
       <div className={cn("flex min-w-full flex-col gap-2", isLast && "pb-8")}>
         <MessageContent
-          className="prose dark:prose-invert relative min-w-full bg-transparent p-0"
+          className={cn(
+            "prose dark:prose-invert relative min-w-full bg-transparent p-0",
+            "prose-h1:scroll-m-20 prose-h1:text-2xl prose-h1:font-semibold prose-h2:mt-12 prose-h2:scroll-m-20 prose-h2:text-xl prose-h2:mb-3 prose-h2:font-medium prose-h3:scroll-m-20 prose-h3:text-base prose-h3:font-medium prose-h4:scroll-m-20 prose-h5:scroll-m-20 prose-h6:scroll-m-20 prose-strong:font-medium prose-table:block prose-table:overflow-y-auto"
+          )}
           markdown={true}
         >
           {children}
