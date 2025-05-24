acddab3d3ccefcb9fc069700a9466089475724c4
ibelick <julien.thibeaut@gmail.com>
2025-05-24 17:16:17 +0200

    feat: add mistral models

--- lib/models/index.ts
+++ lib/models/index.ts
@@ -1,6 +1,5 @@
+import { mistralModels } from "./data/mistral"
 import { openaiModels } from "./data/openai"
+import { ModelConfig } from "./types"
 
-export const MODELS: ModelConfig[] = [
-  ...openaiModels,
-  // ...
-]
+export const MODELS: ModelConfig[] = [...openaiModels, ...mistralModels]
