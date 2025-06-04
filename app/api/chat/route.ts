b6fbe8595d3c7d0f3e9f033ff0cf07f4e9c68c26
ibelick <julien.thibeaut@gmail.com>
2025-06-04 22:32:58 +0200

    feat: add metas icons, add load openrouter models

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -3,7 +3,7 @@ import { SYSTEM_PROMPT_DEFAULT } from "@/lib/config"
 import { loadMCPToolsFromURL } from "@/lib/mcp/load-mcp-from-url"
 import { getAllModels } from "@/lib/models"
 import { getProviderForModel } from "@/lib/openproviders/provider-map"
-import { Provider } from "@/lib/user-keys"
+import type { ProviderWithoutOllama } from "@/lib/user-keys"
 import { Attachment } from "@ai-sdk/ui-utils"
 import { Message as MessageAISDK, streamText, ToolSet } from "ai"
 import {
@@ -74,6 +74,8 @@ export async function POST(req: Request) {
     const allModels = await getAllModels()
     const modelConfig = allModels.find((m) => m.id === model)
 
+    console.log("modelConfig", modelConfig)
+
     if (!modelConfig || !modelConfig.apiSdk) {
       throw new Error(`Model ${model} not found`)
     }
@@ -104,7 +106,8 @@ export async function POST(req: Request) {
       const { getEffectiveApiKey } = await import("@/lib/user-keys")
       const provider = getProviderForModel(model)
       apiKey =
-        (await getEffectiveApiKey(userId, provider as Provider)) || undefined
+        (await getEffectiveApiKey(userId, provider as ProviderWithoutOllama)) ||
+        undefined
     }
 
     const result = streamText({
