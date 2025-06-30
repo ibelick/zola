f87ceaea117ea35132232fe08cf4a642c6eb73b6
ibelick <julien.thibeaut@gmail.com>
2025-06-30 11:00:39 +0200

    feat: add lib/user/types

--- lib/user/api.ts
+++ lib/user/api.ts
@@ -1,17 +1,10 @@
-import type { Tables } from "@/app/types/database.types"
 import { isSupabaseEnabled } from "@/lib/supabase/config"
 import { createClient } from "@/lib/supabase/server"
 import {
   convertFromApiFormat,
   defaultPreferences,
-  type UserPreferences,
 } from "@/lib/user-preference-store/utils"
-
-export type UserProfile = {
-  profile_image: string
-  display_name: string
-  preferences?: UserPreferences
-} & Tables<"users">
+import type { UserProfile } from "./types"
 
 export async function getSupabaseUser() {
   const supabase = await createClient()
